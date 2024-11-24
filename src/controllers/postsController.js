125a55345d3e8e40bf8b5886fd25cc04d4dbec08
João Almeida <joaolucasqa2304@gmail.com>
2024-11-24 19:02:00 -0300

    Atualizacao das fotos
    Atualizacao das fotos utilizadas no projeto, sendo retiradas as antigas e adicionadas outras duas.

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -45,10 +45,9 @@ export async function uploadImagem(req, res) {
 export async function atualizarNovoPost(req, res) {
   const id = req.params.id;
   const urlImagem = `http://localhost:3000/${id}.png`;
-
   try {
-    const imageBuffer = fs.readFileSync(`uploads/${id}.png`);
-    const descricao = await gerarDescricaoComGemini(imageBuffer);
+    const imgBuffer = fs.readFileSync(`uploads/${id}.png`);
+    const descricao = await gerarDescricaoComGemini(imgBuffer);
 
     const post = {
       imgUrl: urlImagem,
